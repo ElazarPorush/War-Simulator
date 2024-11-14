1e243c95b2bafa00687e91448c4ec0ad0134d0eb
Elazar Porush <lzrprws@gmail.com>
2024-11-14 12:29:37 +0200

    Logout button

--- client/src/components/pages/ControlPanel.tsx
+++ client/src/components/pages/ControlPanel.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom'
 import { fetchAttacks } from '../../redux/slices/attackSlice'
 import { socket } from '../../main'
 import AttackCard from './AttackCard'
+import userSlice from '../../redux/slices/userSlice'
 
 export default function ControlPanel() {
   const dispatch = useAppDispatch()
@@ -21,25 +22,36 @@ export default function ControlPanel() {
 
   const handleAttack = () => {
     socket.emit("attack", { missileName: "Fajr-5", from: "Hezbollah", to: "IDF - South" })
+    dispatch(fetchAttacks())
   }
+
+  const handleLogout = () => {
+    dispatch(userSlice.actions.logout());
+    localStorage.removeItem("authorization");
+    navigate('/login')
+  };
 
   return (
     <div>
       <div className='navBar'>
-          {user?.organization.name[1] === "D"? 
-          user?.organization.resources.map(missile => <p>{missile.name} x {missile.amount}</p>) :
-          user?.organization.resources.map(missile => <a>{missile.name} x {missile.amount}</a>)
-          }
-          <button>Log Out</button>
+        <h3>{user?.organization.name}</h3>
+        {user?.organization.name.split(" ")[0] === "IDF" ?
+          user?.organization.resources.map(missile => <p>{missile.name} x {missile.amount}</p>) : 
+          
+          user?.organization.resources.map(missile => <a onClick={handleAttack}>{missile.name} x {missile.amount}</a>)
+        }
+        <button onClick={handleLogout}>Log Out </button>
       </div>
-      <table>
-        <tr>
-          <th>Missile Name</th>
-          <th>Time To Left</th>
-          <th>Status</th>
-        </tr>
-        {attacks?.map(attack => <AttackCard attack={attack} />)}
-      </table>
+      <div className='table'>
+        <table>
+          <tr>
+            <th>Missile Name</th>
+            <th>Time To Left</th>
+            <th>Status</th>
+          </tr>
+          {attacks?.map(attack => <AttackCard attack={attack} />)}
+        </table>
+      </div>
     </div>
   )
 }
